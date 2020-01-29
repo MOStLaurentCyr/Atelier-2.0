4dd676878bc6aa20460d7b6261192f0ae830bb8a
Michaël Tremblay <mic.tremblay11@gmail.com>
2020-01-29 02:03:36 -0500

    Base movement component
    Added base movement component
    Sprint is not working yet
    Added .editorconfig to configure some ReSharper settings

---
+++ src/Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    public bool Sprinting = false;
+    public float WalkSpeed = 10.0f;
+    public float SprintMultiplier = 2f;
+    public float JumpForce = 4.0f;
+
+    private Rigidbody _rb;
+    private Vector2 _moveVector;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+        _moveVector = new Vector2();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 displacement = new Vector3(_moveVector.x, 0, _moveVector.y) * WalkSpeed * Time.fixedDeltaTime;
+
+        if (Sprinting)
+            displacement *= SprintMultiplier;
+
+        _rb.MovePosition(transform.position + displacement);
+
+        // If the player is too slow or stopped, cancel the sprint (for toggle sprint type).
+        if (Sprinting && _rb.velocity.magnitude <= 0.1)
+            Sprinting = false;
+    }
+
+    public void Move(Vector2 value)
+    {
+        _moveVector = value;
+    }
+}
---
+++ src/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private MovementComponent _movementComponent;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        UpdateComponents();
+    }
+
+    public void UpdateComponents()
+    {
+        _movementComponent = GetComponent<MovementComponent>();
+    }
+
+    #region Movement Component Actions
+
+    private void OnMove(InputValue value)
+    {
+        if (!_movementComponent) return;
+
+        var v = value.Get<Vector2>();
+        _movementComponent.Move(v);
+    }
+
+    private void OnJump()
+    {
+        Debug.Log("Jumping");
+    }
+
+    private void OnSprint(InputValue value)
+    {
+        if (!_movementComponent) return;
+
+        // Check the state of the button when this event was called.
+        if (!value.isPressed)
+            // The button was release, so we assume it was a "hold" type (ex: hold Left Shift to sprint).
+            _movementComponent.Sprinting = false;
+        else
+            // We just toggle the value.
+            _movementComponent.Sprinting = !_movementComponent.Sprinting;
+    }
+
+    #endregion
+
+    private void OnLook(InputValue value)
+    {
+        var v = value.Get<Vector2>();
+    }
+
+    private void OnInteract()
+    {
+
+    }
+
+    private void OnBasicAttack()
+    {
+
+    }
+
+    private void OnMelee()
+    {
+
+    }
+
+    private void OnRange()
+    {
+
+    }
+
+    private void OnAbility()
+    {
+
+    }
+}
