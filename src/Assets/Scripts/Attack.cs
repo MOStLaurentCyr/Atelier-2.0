8f05c43a4641e2cfb9a84b8840aec42de720c241
Cthulhu <maxime_morette@hotmail.com>
2020-01-30 18:19:16 -0500

    Changed the attacks to scriptable objects

--- src/Assets/Scripts/Attack.cs
+++ src/Assets/Scripts/Attack.cs
@@ -2,27 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour
+public class Attack : ScriptableObject
 {
-    public Attack CreateAttack(int damage, int range, int cooldown, Effect effect, int level, int costToUpgrade)
+    public static Attack CreateInstance(int damage, int range, int cooldown, Effect effect, int level, int costToUpgrade)
     {
-        var attack = new Attack
+        var attack = ScriptableObject.CreateInstance<Attack>();
         {
-            Damage = damage,
-            Range = range,
-            Cooldown = cooldown,
-            _effect = effect,
-            Level = level,
-            CostToUpgrade = costToUpgrade
+            attack.Damage = damage;
+            attack.Range = range;
+            attack.Cooldown = cooldown;
+            attack._effect = effect;
+            attack.Level = level;
+            attack.CostToUpgrade = costToUpgrade;
         };
         return attack;
     }
 
-    private int Damage;
-    private int Range;
-    private int Cooldown;
-    private int Level;
-    private int CostToUpgrade;
+
+    public int Damage;
+    public int Range;
+    public int Cooldown;
+    public int Level;
+    public int CostToUpgrade;
     private Effect _effect;
 
     public enum Effect
