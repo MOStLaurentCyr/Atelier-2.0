33cedfd72d92af17ca8a4005b3782914436b98d6
Michaël Tremblay <mic.tremblay11@gmail.com>
2020-01-30 15:58:59 -0500

    Added camera movement
    Camera can now move around the player using the mouse or a joystick.
    The camera also collide with everything, but no margin is added for now (so the camera is inside the obstacle).

--- src/Assets/Scripts/CameraController.cs
+++ src/Assets/Scripts/CameraController.cs
@@ -5,44 +5,50 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject Target;
+    public GameObject CameraBoom;
 
-    public float CameraOffset = 4.0f;
-    public float CameraMoveSpeed = 120.0f;
-    public float CameraSensitivity = 10.0f;
+    public float CameraDistance = 4.0f;
+    public float CameraDistanceSmoothness = 100.0f;
     public float CameraMaxAngle = 80.0f;
     public float CameraMinAngle = -80.0f;
+    public LayerMask CameraCollisionMask;
 
     private Vector2 _lookVector;
-    private Vector2 _rotation;
+    private Vector3 _rotation;
+    private Vector3 _cameraPosition;
 
     private void Start()
     {
-        _rotation = transform.localRotation.eulerAngles;
+        // Automatically find the camera boom, camera collision and the player
+        var parentT = transform.parent;
+
+        if (!CameraBoom && parentT)
+            CameraBoom = parentT.gameObject;
+
+        if (!Target && parentT)
+        {
+            var parentParentT = parentT.parent;
+            if (parentParentT)
+                Target = parentParentT.gameObject;
+        }
+
+
+        _rotation = CameraBoom.transform.rotation.eulerAngles;
 
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void Update()
-    {
-        //_rotation.y += _lookVector.x * Time.deltaTime;
-        //_rotation.x += _lookVector.y * Time.deltaTime;
-
-        //_rotation.x = Mathf.Clamp(_rotation.x, CameraMinAngle, CameraMaxAngle);
-
-        //transform.rotation = Quaternion.Euler(_rotation.x, _rotation.y, 0.0f);
-    }
-
     private void LateUpdate()
     {
-        //_rotation.y += _lookVector.x * Time.deltaTime;
-        //_rotation.x += _lookVector.y * Time.deltaTime;
+        _rotation.y += _lookVector.x * Time.deltaTime;
+        _rotation.x += _lookVector.y * Time.deltaTime;
+        _rotation.x = Mathf.Clamp(_rotation.x, CameraMinAngle, CameraMaxAngle);
 
-        //_rotation.x = Mathf.Clamp(_rotation.x, CameraMinAngle, CameraMaxAngle);
+        CameraBoom.transform.rotation = Quaternion.Euler(_rotation.x, _rotation.y, 0.0f);
 
-        //transform.rotation = Quaternion.Euler(_rotation.x, _rotation.y, 0.0f);
-
-        transform.LookAt(Player.transform);
+        OccludeCamera();
+        transform.localPosition = Vector3.Lerp(transform.localPosition, _cameraPosition, Time.deltaTime * CameraDistanceSmoothness);
     }
 
     public void Look(Vector2 value)
@@ -50,10 +56,22 @@ public class CameraController : MonoBehaviour
         _lookVector = value;
     }
 
+    private void OccludeCamera()
+    {
+        _cameraPosition =
+            Physics.Linecast(Target.transform.position, transform.position, out var wallHit, CameraCollisionMask)
+                ? new Vector3(0, 0, -wallHit.distance)
+                : new Vector3(0, 0, -CameraDistance);
+    }
+
+    #region Player Input Events
+
     private void OnLook(InputValue value)
     {
         var v = value.Get<Vector2>();
 
         Look(v);
     }
+
+    #endregion
 }
